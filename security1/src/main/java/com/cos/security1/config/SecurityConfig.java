31364fac7ce3bebc3d0d11644182bc4a7b46e050
Dapui <odh550@naver.com>
2025-03-27 23:14:37 +0900

    구글 회원 프로필 정보 받아보기

--- security1/src/main/java/com/cos/security1/config/SecurityConfig.java
+++ security1/src/main/java/com/cos/security1/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.cos.security1.config;
 
+import com.cos.security1.config.auth.PrincipalOauth2UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -9,10 +11,17 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
+// 1. 코드받기(인증), 2. 엑세스토큰(권한)
+// 3. 사용자프로필 정보를 가져오고 4-1. 그 정보를 토대로 회원가입을 자동으로 진행시키기도 함
+// 4-2. (이메일, 전화번호, 이름, 아이디) 쇼핑몰 -> (집주소), 백화점몰 -> (vip등급, 일반등급)
+
 @Configuration // SecurityConfig 클래스가 bean 메서드를 가지고 있음을 명시
 @EnableWebSecurity  // 스프링 시큐리티 필터(SecurityConfig)가 스프링 필터체인에 등록이 됨
 @EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true) // Spring Security에서 메서드 단위로 권한을 검사할 수 있도록 활성화(@Secured, @PreAuthorize & @PostAuthorize 어노테이션 활성화)
 public class SecurityConfig {
+
+    @Autowired
+    PrincipalOauth2UserService principalOauth2UserService;
 
     // 보안 설정 구성
     @Bean
@@ -30,7 +39,8 @@ public class SecurityConfig {
                 .loginProcessingUrl("/login") // login 주소가 호출되면 시큐리티가 낚아채서 대신 로그인을 진행
                 .defaultSuccessUrl("/")) // 로그인 완료시 이동되는 페이지
             .oauth2Login(oauth2 -> oauth2
-                .loginPage("/loginForm") // 구글 로그인이 완료된 뒤의 후처리가 필요함
+                .loginPage("/loginForm") // 구글 로그인이 완료된 뒤의 후처리가 필요함. Tip. 코드X (엑세스토큰+사용자프로필정보O)
+                .userInfoEndpoint(endpoint -> endpoint.userService(principalOauth2UserService))
             );
         return http.build();
     }
